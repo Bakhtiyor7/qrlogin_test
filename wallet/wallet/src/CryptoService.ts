a679c8aeb8b0cf9487eb701b081865c28550df3a
BAKHTIYOR AKHATOV <85681712+Bakhtiyor7@users.noreply.github.com>
2024-05-24 14:24:47 +0900

    ✨ feat: add encrypt, decrypt functions

--- wallet/wallet/src/CryptoService.ts
+++ wallet/wallet/src/CryptoService.ts
@@ -1,5 +1,6 @@
 import { ec as EC } from "elliptic";
 import { io } from "socket.io-client";
+import CryptoJS from "crypto-js";
 class CryptoService {
   private ec: EC;
   roomId: any;
@@ -16,15 +17,28 @@ class CryptoService {
       publicKey,
     };
   }
+
   public sign(msg: string, prKey: string) {
     const key = this.ec.keyFromPrivate(prKey, "hex");
     const signature = key.sign(msg);
     return signature;
   }
-  // public verify(msg: string, sig: EC.Signature, pubKey: string) {
-  //   const key = this.ec.keyFromPublic(pubKey, "hex");
-  //   const verified = key.verify(msg, sig);
-  //   return verified;
-  // }
+
+  public verify(msg: string, sig: EC.Signature, pubKey: string) {
+    const key = this.ec.keyFromPublic(pubKey, "hex");
+    const verified = key.verify(msg, sig);
+    return verified;
+  }
+
+  public encrypt(text: string, key: string) {
+    const encrypted = CryptoJS.AES.encrypt(text, key).toString();
+    return encrypted;
+  }
+
+  public decrypt(encryptedText: string, key: string) {
+    const bytes = CryptoJS.AES.decrypt(encryptedText, key);
+    const decrypted = bytes.toString(CryptoJS.enc.Utf8);
+    return decrypted;
+  }
 }
 export default new CryptoService();
